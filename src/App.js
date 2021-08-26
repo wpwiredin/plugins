db781d7a63460692b5147222439b5e082a961227
williammabotjadev <williammabotjadev@gmail.com>
2021-08-26 16:54:19 +0200

    logo placement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo-plugins.png';
 import './App.css';
 
+const LogoStyle = {
+  marginTop: '160px',
+  marginLeft: '360px'
+}
+
 function App() {
   return (
     <div className="App">
-        <img src={logo} alt={"logo"} />
+        <img src={logo} alt={"logo"} style={LogoStyle} />
     </div>
   );
 }
