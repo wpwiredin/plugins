44c41a84beaa5d7fa8d852d5a3a112f5cbf526d9
williammabotjadev <williammabotjadev@gmail.com>
2021-08-30 12:40:33 +0200

    Paypal Form

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ function App() {
   return (
     <div className="App">
         <img src={logo} alt={"logo"} style={LogoStyle} />
+        <br />
+        <form action="https://www.paypal.com/donate" method="post" target="_top">
+            <input type="hidden" name="hosted_button_id" value="U3WFXQUMPU9NN" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_ZA/i/scr/pixel.gif" width="1" height="1" />
     </div>
   );
 }
